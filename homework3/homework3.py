869835652c43c8c4703dad62069484f369da3f22
Айгиз Асфандияров <aigizasfandiyarov@gmail.com>
2024-12-01 20:34:21 +0500

    Add files via upload

--- homework3/homework3.py
+++ homework3/homework3.py
@@ -7,18 +7,23 @@ def get_decimal_number():
             print("Ошибка: пожалуйста, введите положительное целое число.")
     except ValueError:
         print("Ошибка: пожалуйста, введите целое число.")
+
+def decimal_to_ternary(decimal_number):
+    if decimal_number == 0:
+        return "0"
     
+    ternary_number = ""
+    while decimal_number > 0:
+        ternary_number = str(decimal_number % 3) + ternary_number
+        decimal_number //= 3
+    
+    return ternary_number
 
-
-def decimal_to_ternary(n):
-    if n == 0:
-        return ""
-    return str(n % 3) + decimal_to_ternary(n // 3)
 def convert_number():
     decimal_number = get_decimal_number()
     ternary = decimal_to_ternary(decimal_number)
     result = ternary
     return result
 
-# Выполнение функции
 print(convert_number())
+
